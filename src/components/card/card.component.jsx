3434dae0b50b22c6cbfab41b7a12b4b5c452581e
Oluwapelumi Alabi <alabideborah973@gmail.com>
2021-08-13 19:40:30 +0100

    monster

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './card.styles.css';
+export const Card = props => (
+    <div className="card-container">
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=100x80`}/>
+        <h2> {props.monster.name } </h2>
+        <h1> {props.monster.email } </h1>
+    </div>
+)
